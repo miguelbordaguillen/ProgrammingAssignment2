19f1f8dd3b898b084eaf7449060c61896941b125
miguelbordaguillen <57612190+miguelbordaguillen@users.noreply.github.com>
2020-01-16 02:20:35 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Creating conditions to calculate an inverse matrix: Square matrix
 
-## Write a short comment describing this function
+MakeCacheMatrix <- function(square){
+  X <- as.matrix(square)
+  
+  
+  if(ncol(X)>=3 | (nrow(X)>=3)) {
+    stop('Theres not square matrix')
+  }
+  if(ncol(X)==1){
+    stop('Is a vector')
+  }
+  if(ncol(X)==2){
+    
+##We have to get the determinant (Y) of the matrix and test the last condition Y==0
+    a <- X[1]
+    b <- X[3]
+    c <- X[2]
+    d <- X[4]
+    Y <- a * d - b * c
+    if(Y==0){
+      stop("Determinant = 0 means inverse matrix doesnt exist")}
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+##And now, we could get each element of the inverted matrix  
+    A <- d/Y
+    B <- -b/Y
+    C <- -c/Y
+    D <- a/Y
+    
+##Finally, we're gonna create an inverted matrix 
+    
+    newmatrix <- as.matrix(cbind(c(A,C), c(B,D)))
+    }
+  }
